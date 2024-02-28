447c2807b6f0ec9c3b174cbcc2ebef716ad43692
javvadasghar <muhammad-jawad.asghar@tu-ilmenau.de>
2024-02-28 14:14:41 +0100

    profile pic change

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ class About extends Component {
               <div className="polaroid">
                 <span style={{ cursor: "auto" }}>
                   <img
-                    height="250px"
+                    height="400px"
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
